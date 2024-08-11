8699559cedc0ed049cd17a45ded10d09997f19b7
Lydia Zamora <lydiazamorayoga@gmail.com>
2024-08-10 17:41:52 -0700

    exercise finished

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LINQ
+﻿using System.Linq;
+using System.Collections.Generic;
+namespace LINQ 
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favBooks = new List<string>()
+            { "slaughterhouse five", "harry potter and prisoner of azkaban", "narrow road to the deep north", "catch 22", "atlas shrugged"};
+
+            var booksByLength = favBooks.OrderBy(book => book.Length);
+
+            foreach(var item in booksByLength)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
